c196485880699618b1ee487afc2883a66af9da08
t-kusakabe <ttmac0704@gmail.com>
2022-04-19 19:15:39 +0900

    named export

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Task from './Task';
+import { Task } from './Task';
 
 export const TaskList = ({ loading, tasks, onPinTask, onArchiveTask }) => {
   const events = {
